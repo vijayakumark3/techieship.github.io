ea028a7798d9ea60bf737a471a576d8231f54354
Vijayakumar K <119727353+vijayakumark3@users.noreply.github.com>
2022-12-03 20:00:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AadukalamXS - Blog for Tech, Gaming, Gadgets</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<script src="https://kit.fontawesome.com/e370f778c1.js" crossorigin="anonymous"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<nav>
+		<h2>AadukalamXS</h2>
+
+		<ul>
+			<li><a href="#contact">Contact</a></li>
+			<li><a target="_blank" href="https://paypal.me/vijayakumark3?country.x=IN&locale.x=en_GB">Donate Us</a></li>
+		</ul>
+		<div class="cleardiv"></div>
+	</nav>
+
+	<div class="header">
+		<h3>Top Gadget</h3>
+		<h5>Best 7 In-Ear Headphones under 2000 Rupees</h5>
+		<div class="contentContainer">
+			<div class="content">
+				<p class="pname">1. 1MORE Piston Fit Wired in Ear Earphone with Mic</p>
+				<p class="summary">
+					Ergonomic Version Of Our Award-Winning Piston Classic -oblique angled ear fittings naturally match your ear canals. 3 sets of included ear tip sizes ensure a proper fit for all. They€re more comfortable and less likely to fall out, freeing you to enjoy your music
+					<br><br>
+					Retro Meets Modern Design-iPhone matched colors on an aluminum alloy body merges a timeless esthetic with your current device
+					<br><br>
+					Dynamic Driver-dual layer composite driver with aerospace grade titanium delivers sizzling highs, present mids, and powerful bass
+					<br><br>
+					Tuned By A Grammy Winning Sound Engineer-1MORE collaborated with internationally acclaimed sound engineer Luca Bignardi to perfect the final tuning to deliver a precise representation of your favorite artist€s intended sound
+					<br><br>
+					Android and iOS Compatible MEMS Microphone -in-line microphone compatible with Apple (iPhone/iPad) and Android allows you to take calls without cross-talk or background static
+				</p>
+				<a target="_blank" href="https://amzn.to/3j57qRx">Check out</a>
+			</div>
+
+			<div class="content">
+				<p class="pname">2. realme Buds 2 Wired in Ear Earphones with Mic</p>
+				<p class="summary">
+					Enjoy the powerful 11.2mm bass boost driver which consist of multi-layer composite diaphragm, bringing you a deep and powerful, yet accurate bass response.
+					<br><br>
+					The inline remote features three tactile buttons and a mic, so you can control your music and videos, incoming calls, and even summon your voice assistant directly at the touch of a button.
+					<br><br>
+					A premium, reinforced braided jacket, and two evenly grooved TPU cables make for a design that is robust and durable.
+					<br><br>
+					Realme buds 2 features built-in magnets and a cable strap which are designed to provide the ultimate solution for neatly storing your earphones. Impedance: 32Ω, Rated Power: 3mW, Sound Pressure Level: 106dB
+					<br><br>
+					Add a touch of style to your music experience with the realme Buds 2. The matte, streamlined design looks elegant and attractive.
+					<br><br>
+					Warranty: Realme Buds 2 is covered under 6 months brand Warranty against any manufacturing defects
+					<br><br>
+					Country of Origin: China
+				</p>
+				<a target="_blank" href="https://amzn.to/37iDdfl">Check out</a>
+			</div>
+
+			<div class="content">
+				<p class="pname">3. MI Dual Driver Wired in Ear Earphones with Mic</p>
+				<p class="summary">
+					The dual dynamic driver structure brings a quality sound experience with the drivers delivering rich bass and crisp treble
+					<br><br>
+					Dual drivers ensure low distortion and focus on all frequencies to deliver the complete sound experience whereas a single driver focuses on a single frequency range and distorts at high volumes
+					<br><br>
+					10mm & 8mm Dynamic Drivers which produces Crisp Vocals & Rich Bass, Dual Dynamic Drivers
+					<br><br>
+					Passive Noise Cancellation. Impedance: 32Ω Jack Type : 3.5mm
+					<br><br>
+					3-button operation for better convenience and operability. Trigger the voice assistant by long pressing the play/pause button
+					<br><br>
+					Tangle-Free Braided Cable. Frequency Response Range : 20-40000Hz
+					<br><br>
+					Soft comfortable anti-slip earplugs perfectly fit the contour of your ears offering exceptional comfort
+				</p>
+				<a target="_blank" href="https://amzn.to/3JbP9MX">Check out</a>
+			</div>
+
+			<div class="content">
+				<p class="pname">4. Sony MDR-EX150AP Wired In Ear Headphone with Mic</p>
+				<p class="summary">
+					In-line mic for hands-free calling, Y-shaped cord type:1.2m length. Sensitivities (dB/mW) 103dB / mW
+					<br><br>
+					Lightweight for ultimate music mobility
+					<br><br>
+					Comfortable, secure-fitting silicone earbuds for long listening hours
+					<br><br>
+					Match your style with vivid colors<br>
+					Shiny metallic finish housing<br>
+					Frequency Range: 5Hz to 24kHz. 1 year manufacturer warranty
+					Unit of Microphone:Electret Condenser
+				</p>
+				<a target="_blank" href="https://www.amazon.in/dp/B015ZDGEQU/?coliid=I1COYHNW1IG6K4&colid=1Y8NI15ZX70JL&psc=0&ref_=lv_ov_lig_dp_it">Check out</a>
+			</div>
+
+			<div class="content">
+				<p class="pname">5. JBL C100SI Wired In Ear Headphones with Mic, JBL Pure Bass Sound, One Button Multi-function Remote, Angled Buds for Comfort fit</p>
+				<p class="summary">
+					JBL Signature Sound
+					Lightweight and Comfortable : The 3 sizes of ear tips (S,M,L) that are included allow you to choose a size that gives you the most comfortable listening experience even for longer listening periods
+					<br><br>
+					JBL Signature Sound. Frequency range:20-20kHz,Driver sensitivity:100±3dBSPL, 1mW,Maximum SPL:5mW
+					<br><br>
+					Extra Deep Bass. Troubleshooting steps : Kindly ensure 3.5mm port on Host device is clean and dust free and 3.5mm jack of the earphone is adequately inserted inside the input device port
+					<br><br>
+					Noise Cancelling Microphone
+					<br><br>
+					One-Button Universal Remote with Mic
+					<br><br>
+					Quick Launch Access to Google Assistant / Siri
+				</p>
+				<a target="_blank" href="https://www.amazon.in/dp/B01DEWVZ2C/?coliid=ID35J6GQPKPQR&colid=1Y8NI15ZX70JL&ref_=lv_ov_lig_dp_it&th=1">Check out</a>
+			</div>
+
+			<div class="content">
+				<p  class="pname">6. Sennheiser CX 180 Street II In-Ear Headphone</p>
+				<p class="summary">
+					Note: Item is a Wired in-the-ear headphone without microphone/ button to control volume
+					<br><br>
+					Powerful, bass-driven stereo sound. Inline Remote: No
+					<br><br>
+					Provides unparalleled bass sound with balanced and high resolution stereo sound and minimizes the outside ambient noise for better listening
+					<br><br>
+					Innovative finger-contoured housing design enables for easy adjustment and optimal wearing comfort
+					<br><br>
+					Optimised for mp3, iPod, iPhone, CD players and portable gaming systems
+					<br><br>
+					Frequency response (Headphones) - 20 Hz - 20,000 Hz
+					<br><br>
+					Sound pressure level (SPL) - 110dB (1kHz/1Vrms)
+				</p>
+				<a target="_blank" href="https://www.amazon.in/dp/B00D75AB6I/?coliid=I2ZP5UCXYQE9R3&colid=1Y8NI15ZX70JL&ref_=lv_ov_lig_dp_it&th=1">Check out</a>
+			</div>
+
+			<div class="content">
+				<p class="pname">7. boAt Bassheads 225 in Ear Wired Earphones with Mic</p>
+				<p class="summary">
+					Innovative housing design enables for easy adjustment and optimal wearing comfort with powerful 10mm drivers for sonic clarity, bass-driven stereo sound and good attenuation of ambient noise
+					<br><br>
+					The noise-cancelling microphone enables trouble free receiving of calls or undeterred sessions of musical extravaganza
+					<br><br>
+					Made to fit you and your lifestyle, providing a smooth grip for an extra secure and comfortable fit for your ears
+					<br><br>
+					Passive noise cancellation feature enables users to enjoy listening to music even in a chaotic and noisy environment; it also enables users to receive calls on-the-go
+					<br><br>
+					Crafted with care, the polished metal ensures its longevity, the tangle-free cable is lightweight and features user-friendly controls for easy functioning
+					<br><br>
+					Compatible with all devices having 3.5mm jack.
+					<br><br>
+					1 year warranty from the date of purchase
+				</p>
+				<a target="_blank" href="https://www.amazon.in/dp/B01M9C51T9/?coliid=I2E35JQ18ZGZU1&colid=1Y8NI15ZX70JL&ref_=lv_ov_lig_dp_it&th=1">Check out</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		<div class="aboutUs">
+			<h2>ABOUT US</h2>
+			<p>Hello Guys!
+				<br>
+				This is a website of aadukalam youtube channel this website about new and trending gadgets, tech, and gaming news focused blog, I'm a village battle royale and esports gamer in India and I'm uploading more gameplay videos and live on this channel.
+				If you like it, do subscribe and support :)
+				<br>
+				This channel mainly focused on battle royale games like free fire, pubg, fornite, call of duty, etc,.</p>
+		</div>
+		<div class="socialLinks"  id="contact">
+			<h2>CONTACT & FOLLOW US</h2>
+			<ul>
+				<li><a target="_blank"><i class="fa-solid fa-envelope"></i> contact.vijayakumar@gmail.com</a></li>
+				<li><a target="_blank" href="https://www.youtube.com/@aadukalamxs"><i class="fa-brands fa-youtube"></i> YouTube</a></li>
+				<li><a target="_blank" href="https://www.twitch.com/aadukalamxs"><i class="fa-brands fa-twitch"></i> Twitch</a></li>
+				<li><a target="_blank" href="https://www.facebook.com/aadukalamxs"><i class="fa-brands fa-facebook"></i> Facebook</a></li>
+				<li><a target="_blank" href="https://www.instagram.com/aadukalamxs"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
+				<li><a target="_blank" href="https://www.twitter.com/aadukalamxs"><i class="fa-brands fa-twitter"></i> Twitter</a></li>
+			</ul>
+		</div>
+		<div class="cleardiv"></div>
+	</div>
+
+
+	<div class="copyright">
+		<p>Copyright &copy; 2022 | Designed by <a target="_blank" href="https://vijayakumark.000webhostapp.com/">Vijayakumar K</a></p>
+	</div>
+
+</body>
+</html>
